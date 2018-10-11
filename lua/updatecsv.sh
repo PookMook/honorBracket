b4fb838872f4f5c9d62275a15fd24a9aba438a92
PookMook <arthur.juchereau@gmail.com>
2018-10-11 13:25:03 -0400

    Updating currentWeek.csv

--- lua/updatecsv.sh
+++ lua/updatecsv.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #Get file
+echo "Starting Crontab job for honorBracket at `date`" >> /var/log/crontab
 cd "/mnt/d/wow/World of Warcraft Classic"
 lua5.3 tocsv.lua
 mv export/currentWeek.csv ~/git/honorBracket/src/data
