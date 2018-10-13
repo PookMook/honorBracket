700ad28776569434c62b354be44d20aca539189b
Arthur <arthur.juchereau@gmail.com>
2018-10-13 11:23:10 -0400

    ergo

--- gatsby/src/components/konvaRank.js
+++ gatsby/src/components/konvaRank.js
@@ -1,41 +1,39 @@
-import React, { Component } from 'react';
-import Konva from 'konva';
-import { Stage, Layer, Rect, Text, Line } from 'react-konva';
-
+import React from 'react'
+import { Stage, Layer, Rect, Text, Line } from 'react-konva'
 
 const RankLine = (props) => (
   <Stage width={960} height={300}>
     <Layer>
-      <Text text='Ranking Points (RP, lower) required per rank (upper)' fill='yellow'/>
+      <Text text='Ranking Points (RP, lower) required per rank (upper)' fill='yellow' />
       <Rect
         x={5}
         y={60}
         width={910}
         height={1}
-        fill="yellow"
+        fill='yellow'
       />
-      {brackets.map((bracket,i) => (
+      {brackets.map((bracket, i) => (
         <Rect
-          x={5+(bracket * 14)}
+          x={5 + (bracket * 14)}
           y={55}
           width={1}
           height={10}
           fill='yellow'
         />
       ))}
-      {brackets.map((bracket,i) => (
+      {brackets.map((bracket, i) => (
         <Text
           x={(bracket * 14)}
           y={65}
-          text={bracket+"k"}
+          text={bracket + 'k'}
           fill='yellow'
         />
       ))}
-      {bracketsShift.map((bracket,i) => (
+      {bracketsShift.map((bracket, i) => (
         <Text
           x={bracket}
           y={45}
-          text={(i+1)}
+          text={(i + 1)}
           fill='yellow'
         />
       ))}
@@ -44,10 +42,10 @@ const RankLine = (props) => (
         x={5}
         y={15}
         points={[
-          (props.state.RP / 1000)*14, 45,
-          middlePoint((props.state.RP / 1000)*14, ((props.state.RP - props.state.decay) / 1000)*14), 0,
-          ((props.state.RP - props.state.decay) / 1000)*14, 45,
-          middlePoint((props.state.RP / 1000)*14, ((props.state.RP - props.state.decay) / 1000)*14), 3]}
+          (props.state.RP / 1000) * 14, 45,
+          middlePoint((props.state.RP / 1000) * 14, ((props.state.RP - props.state.decay) / 1000) * 14), 0,
+          ((props.state.RP - props.state.decay) / 1000) * 14, 45,
+          middlePoint((props.state.RP / 1000) * 14, ((props.state.RP - props.state.decay) / 1000) * 14), 3]}
         tension={1}
         closed
         fill='red'
@@ -68,10 +66,10 @@ const RankLine = (props) => (
         x={5}
         y={60}
         points={[
-          ((props.state.RP - props.state.decay) / 1000)*14, 0,
-          middlePoint(((props.state.RP - props.state.decay) / 1000)*14,((props.state.nextWeekRP) / 1000)*14), 55,
-          ((props.state.nextWeekRP) / 1000)*14, 0,
-          middlePoint(((props.state.RP - props.state.decay) / 1000)*14,((props.state.nextWeekRP) / 1000)*14), 58]}
+          ((props.state.RP - props.state.decay) / 1000) * 14, 0,
+          middlePoint(((props.state.RP - props.state.decay) / 1000) * 14, ((props.state.nextWeekRP) / 1000) * 14), 55,
+          ((props.state.nextWeekRP) / 1000) * 14, 0,
+          middlePoint(((props.state.RP - props.state.decay) / 1000) * 14, ((props.state.nextWeekRP) / 1000) * 14), 58]}
         tension={1}
         closed
         fill='green'
@@ -88,8 +86,6 @@ const RankLine = (props) => (
         closed
         fill='green'
       />
-
-
     </Layer>
   </Stage>
 )
@@ -98,6 +94,6 @@ export default RankLine
 
 const brackets = [0, 2, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65]
 const bracketsShift = [15, 50, 105, 175, 245, 315, 385, 455, 525, 595, 665, 735, 800, 875]
-const middlePoint =  (first, second) => (
+const middlePoint = (first, second) => (
   (first + second) / 2
 )
--- gatsby/src/components/nav.js
+++ gatsby/src/components/nav.js
@@ -3,9 +3,9 @@ import { Link } from 'gatsby'
 
 const Nav = () => (
   <nav id="Nav">
-    <Link to="/" activeStyle={{display:'none'}}>Brackets</Link>
-    <Link to="/standings" activeStyle={{display:'none'}}>Standings</Link>
-    <Link to="/about/" activeStyle={{display:'none'}}>About</Link>
+    <Link to="/" activeStyle={{color: 'white', textDecoration: 'none'}}>Brackets</Link>
+    <Link to="/standings" activeStyle={{color: 'white', textDecoration: 'none'}}>Standings</Link>
+    <Link to="/about/" activeStyle={{color: 'white', textDecoration: 'none'}}>About</Link>
   </nav>
 )
 
