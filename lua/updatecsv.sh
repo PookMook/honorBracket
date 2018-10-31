20343fef02fec4af1f6ba43f3d5450430accc426
PookMook <arthur.juchereau@gmail.com>
2018-10-31 16:20:18 -0400

    Updating currentWeek.csv

--- lua/updatecsv.sh
+++ lua/updatecsv.sh
@@ -23,6 +23,7 @@ cd ~/git/honorBracket/
 if output=$(git status --porcelain) && [ -z "$output" ]; then
 	  # Working directory clean
 	  echo "no changes at `date`" >> /var/log/crontab
+	  sed -E -i "s/lastUpdate\: '.+'/lastUpdates: '$date'/g" gatsby/gatsby-config.js
 else 
 	  echo "uncommitted changesi at `date`, let's go" >> /var/log/crontab
 	    # Uncommitted changes
