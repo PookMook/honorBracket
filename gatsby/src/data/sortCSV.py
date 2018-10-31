e7ae4698ad72a31f5ef7840e0182c91b27152289
PookMook <arthur.juchereau@gmail.com>
2018-10-31 13:56:10 -0400

    Updating currentWeek.csv

---
+++ gatsby/src/data/sortCSV.py
@@ -0,0 +1,10 @@
+import sys, csv ,operator
+data = csv.reader(open('currentWeek.csv'),delimiter=',')
+next(data, None)
+sortedlist = sorted(data, key=lambda row: int(row[1]), reverse=True)    # 1 specifies according to second column we want to sort
+#now write the sorte result into new CSV file
+with open("currentWeekSorted.csv", "w") as f:
+    fileWriter = csv.writer(f, delimiter=',')
+    for row in sortedlist:
+        fileWriter.writerow(row)
+
--- lua/updatecsv.sh
+++ lua/updatecsv.sh
@@ -12,6 +12,14 @@ mv export/currentWeek.csv ~/git/honorBracket/gatsby/src/data
 
 cd ~/git/honorBracket/
 
+cd gatsby/src/data
+python3 sortCSV.py
+echo "name,honor,class,LWHonor,standing,rank,rankProgress,RP" > currentWeek.csv
+cat currectWeekSorted.csv >> currectWeek.csv
+rm currectWeekSorted.csv
+
+cd ~/git/honorBracket/
+
 if output=$(git status --porcelain) && [ -z "$output" ]; then
 	  # Working directory clean
 	  echo "no changes at `date`" >> /var/log/crontab
