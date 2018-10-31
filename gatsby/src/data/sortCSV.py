3d3bde345bb46c64291344c213ca06cabce570e5
PookMook <arthur.juchereau@gmail.com>
2018-10-31 14:32:54 -0400

    Updating currentWeek.csv

--- gatsby/src/data/sortCSV.py
+++ gatsby/src/data/sortCSV.py
@@ -1,7 +1,7 @@
 import sys, csv ,operator
 data = csv.reader(open('currentWeek.csv'),delimiter=',')
 next(data, None)
-sortedlist = sorted(data, key=lambda row: int(row[1]), reverse=True)    # 1 specifies according to second column we want to sort
+sortedlist = sorted(data, key=lambda row: (int(row[1]),row[0]), reverse=True)    # 1 specifies according to second column we want to sort
 #now write the sorte result into new CSV file
 with open("currentWeekSorted.csv", "w") as f:
     fileWriter = csv.writer(f, delimiter=',')
