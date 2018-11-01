91a1083fbefc5c4d7b9483c69a86e03870ae5571
PookMook <arthur.juchereau@gmail.com>
2018-11-01 10:30:05 -0400

    Updating currentWeek.csv

--- gatsby/gatsby-config.js
+++ gatsby/gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Bracket Visualizer',
-    lastUpdate: 'Thu Nov  1 01:30:06 UTC 2018',
+    lastUpdate: 'Thu Nov  1 14:30:05 UTC 2018',
     backgroundColor: '#003366',
     themeColor: '#003366',
     headerColor: '#FFCC00',
